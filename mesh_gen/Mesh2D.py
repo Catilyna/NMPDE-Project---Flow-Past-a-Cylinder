0ce5ab39b0fa88aee4f6e01b0830fb1ae97b5dd7
grimaldiTob <forzamilan688@gmail.com>
2025-12-08 04:18:24 +0100

    Reorganized StationaryNAvierStokes and Mesh2D class

---
+++ mesh_gen/Mesh2D.py
@@ -0,0 +1,138 @@
+import gmsh
+import argparse
+import sys
+import os
+
+class Mesh2D:
+    """
+    Class used to inizialize mesh for a Navier Stokes equation problem
+    """
+    def __init__(self, L, H):
+        self.l = L
+        self.h = H
+        
+        self.lines = []
+        self.circles = []
+        
+        # initialize gmsh
+        gmsh.initialize()
+        gmsh.model.add("dealII_domain")
+        
+        self.box_loop = None
+        self.circle_loop = None
+        
+        self.surface = None
+        
+        
+    def create_rect(self):
+        points = []
+        
+        points[0] = gmsh.model.geo.addPoint(0, 0, 0)
+        points[1] = gmsh.model.geo.addPoint(self.l, 0, 0)
+        points[2] = gmsh.model.geo.addPoint(self.l, self.h, 0)
+        points[3] = gmsh.model.geo.addPoint(0, self.h, 0)
+        
+        self.lines.append(gmsh.model.geo.addLine(points[0], points[1]))
+        self.lines.append(gmsh.model.geo.addLine(points[1], points[2]))
+        self.lines.append(gmsh.model.geo.addLine(points[2], points[3]))
+        self.lines.append(gmsh.model.geo.addLine(points[3], points[0]))
+        
+        # usefull to tell dealII what is what precisely
+        gmsh.model.addPhysicalGroup(1, [self.lines[3]], 0, name="Inlet")
+        gmsh.model.addPhysicalGroup(1, [self.lines[1]], 1, name="Outlet")
+        gmsh.model.addPhysicalGroup(1, [self.lines[0], self.lines[2]], 2, name="Walls")
+        
+        self.box_loop = gmsh.model.geo.addCurveLoop(self.lines)
+        
+    def create_circle(self, cx, cy, r):
+        """
+        Function creates a circle given the (x, y) coordinates and the radius.
+        """
+        c1 = gmsh.model.geo.addPoint(cx, cy, 0)
+        c2 = gmsh.model.geo.addPoint(cx + r, cy, 0)
+        c3 = gmsh.model.geo.addPoint(cx - r, cy, 0)
+        c4 = gmsh.model.geo.addPoint(cx, cy + r, 0)
+        c5 = gmsh.model.geo.addPoint(cx, cy - r, 0)
+
+        # First argument of addCircleArc is the starting point, last one is the ending point. Middle one is the center of rotation
+        self.circles.append(gmsh.model.geo.addCircleArc(c2, c1, c4))
+        self.circles.append(gmsh.model.geo.addCircleArc(c4, c1, c3))
+        self.circles.append(gmsh.model.geo.addCircleArc(c3, c1, c5))
+        self.circles.append(gmsh.model.geo.addCircleArc(c5, c1, c2))
+
+        gmsh.model.addPhysicalGroup(1, self.circles, 3, name="Cylinder")
+
+        self.circle_loop = gmsh.model.geo.addCurveLoop(self.circles)
+        
+    def setSurface(self):
+        self.surface = gmsh.model.geo.addPlaneSurface([self.box_loop, self.circle_loop])
+        gmsh.model.addPhysicalGroup(2, [self.surface], 0, name="Fluid")
+
+    def setFields(self, distMin, distMax, lcMin, lcMax, sampling):   
+        """ 'Fields' are used to define triangulations dimensions given the distance from the cilinder
+            this is true because close to the cylinder, gradients of u and p will modify at high frequency
+            so we need smaller triangulations close to it and bigger ones (where the flow is more steady). """
+        dist_field = gmsh.model.mesh.field.add("Distance")
+        gmsh.model.mesh.field.setNumbers(dist_field, "CurvesList", self.circles)
+        gmsh.model.mesh.field.setNumber(dist_field, "Sampling", sampling)
+
+        # 2. Create a threshold field: 
+        #    - Inside DistMin: Mesh size = LcMin (fine)
+        #    - Outside DistMax: Mesh size = LcMax (coarse)
+        threshold_field = gmsh.model.mesh.field.add("Threshold")
+        gmsh.model.mesh.field.setNumber(threshold_field, "IField", dist_field)
+        gmsh.model.mesh.field.setNumber(threshold_field, "LcMin", lcMin) # Fine mesh near cylinder
+        gmsh.model.mesh.field.setNumber(threshold_field, "LcMax", lcMax)  # Coarse mesh far away
+        gmsh.model.mesh.field.setNumber(threshold_field, "DistMin", distMin)
+        gmsh.model.mesh.field.setNumber(threshold_field, "DistMax", distMax)
+        
+        gmsh.model.mesh.field.setAsBackgroundMesh(threshold_field)
+        
+    def build(self, cx, cy, r, output_filename):
+        self.create_rect()
+        self.create_circle(cx, cy, r)
+        
+        self.setSurface()
+        
+        gmsh.model.geo.synchronize()
+        
+        self.setFields(0.15, 0.5, 0.1, 0.02, 100)       
+        
+        gmsh.model.mesh.generate(2) # Generate 2D mesh
+        gmsh.write("./mesh/" + output_filename + ".msh")
+
+        # Launch GUI to see the result (optional)
+        gmsh.fltk.run()
+
+        gmsh.finalize()
+        
+    
+def main():
+    parser = argparse.ArgumentParser(description="Generate a 2D rectangular mesh for deal.II")
+
+    # Geometric Arguments
+    parser.add_argument('-L', '--length', type=float, default=2.0, 
+                        help='Length of the domain (default: 2.0)')
+    parser.add_argument('-H', '--height', type=float, default=1.0, 
+                        help='Height of the domain (default: 1.0)')
+    
+    parser.add_argument('-cx', '--cx', type=float, default=1.0, 
+                        help='Set the cx of the center ot the cilinder')
+    parser.add_argument('-cy', '--cy', type=float, default=0.5, 
+                        help='Set the cy of the center ot the cilinder')
+    parser.add_argument('-r', '--radius', type=float, default=0.1, 
+                        help='Set the radius of the cilinder')
+
+    # Parse the arguments
+    args = parser.parse_args()
+    
+    mesh = Mesh2D(args.length, args.heigth)
+    mesh.build(args.cx, args.cy, args.radius, "mesh2D_example")
+
+
+if __name__ == "__main__":
+    main()
+
+        
+        
+                
--- src/StationaryNavierStokes.cpp
+++ src/StationaryNavierStokes.cpp
@@ -24,14 +24,21 @@ StationaryNavierStokes<dim>::StationaryNavierStokes(const unsigned int degree)
 	, dof_handler(triangulation)
 {}
 
+/** @brief method responsible for setting up the degrees of freedom (DoFs), 
+ * renumbering them, and applying the boundary conditions before solving 
+ * the linear system.
+ */
 template <int dim>
 void StationaryNavierStokes<dim>::setup_dofs()
 {
+	// clear data
 	system_matrix.clear();
 	pressure_mass_matrix.clear();
+
 	dof_handler.distribute_dofs(fe);
-	std::vector<unsigned int> block_component(dim + 1, 0);
-	block_component[dim] = 1;
+	std::vector<unsigned int> block_component(dim + 1, 0); // assign 0 to all components
+	block_component[dim] = 1; // assign 1 to the pressure component
+	
 	DoFRenumbering::component_wise(dof_handler, block_component);
 	dofs_per_block = DoFTools::count_dofs_per_fe_block(dof_handler, block_component);
 	unsigned int dof_u = dofs_per_block[0];
@@ -61,41 +68,64 @@ void StationaryNavierStokes<dim>::initialize_system()
 		DoFTools::make_sparsity_pattern(dof_handler, dsp, nonzero_constraints);
 		sparsity_pattern.copy_from(dsp);
 	}
+	// code taken from Bucelli's labs
+    std::cout << "  Initializing the system matrix" << std::endl;
 	system_matrix.reinit(sparsity_pattern);
+
+    std::cout << "  Initializing the solution vector" << std::endl;
 	present_solution.reinit(dofs_per_block);
+
 	newton_update.reinit(dofs_per_block);
+
+    std::cout << "  Initializing the system right-hand side" << std::endl;
 	system_rhs.reinit(dofs_per_block);
 }
 
+/** @brief Assembles the whole system. (too lazy) */
 template <int dim>
 void StationaryNavierStokes<dim>::assemble(const bool initial_step, const bool assemble_matrix)
 {
 	if (assemble_matrix)
 		system_matrix = 0;
 	system_rhs = 0;
+
 	const QGauss<dim> quadrature_formula(degree + 2);
 	FEValues<dim> fe_values(fe, quadrature_formula, update_values | update_quadrature_points | update_JxW_values | update_gradients);
+
+	// usefull values referring to dofs and quadrature points
 	const unsigned int dofs_per_cell = fe.n_dofs_per_cell();
 	const unsigned int n_q_points = quadrature_formula.size();
+
+	// istantiate local matrix and vectors
 	const FEValuesExtractors::Vector velocities(0);
 	const FEValuesExtractors::Scalar pressure(dim);
 	FullMatrix<double> local_matrix(dofs_per_cell, dofs_per_cell);
 	Vector<double> local_rhs(dofs_per_cell);
+
 	std::vector<types::global_dof_index> local_dof_indices(dofs_per_cell);
+
+	// vecotrs used to store past values at each quadrature point
 	std::vector<Tensor<1, dim>> present_velocity_values(n_q_points);
 	std::vector<Tensor<2, dim>> present_velocity_gradients(n_q_points);
 	std::vector<double> present_pressure_values(n_q_points);
-	std::vector<double> div_phi_u(dofs_per_cell);
-	std::vector<Tensor<1, dim>> phi_u(dofs_per_cell);
-	std::vector<Tensor<2, dim>> grad_phi_u(dofs_per_cell);
-	std::vector<double> phi_p(dofs_per_cell);
+
+	// vectors used to store values of the test functions
+	std::vector<double> div_phi_u(dofs_per_cell);  			// divergence of velocity
+	std::vector<Tensor<1, dim>> phi_u(dofs_per_cell);       // velocity 
+	std::vector<Tensor<2, dim>> grad_phi_u(dofs_per_cell);  // gradient of velocity
+	std::vector<double> phi_p(dofs_per_cell);               // pressure
+
 	for (const auto &cell : dof_handler.active_cell_iterators()) {
 		fe_values.reinit(cell);
+
 		local_matrix = 0;
 		local_rhs = 0;
+
+		// We need to know the values and the gradient of velocity on quadrature nodes (explained by Bucelli)
 		fe_values[velocities].get_function_values(evaluation_point, present_velocity_values);
 		fe_values[velocities].get_function_gradients(evaluation_point, present_velocity_gradients);
 		fe_values[pressure].get_function_values(evaluation_point, present_pressure_values);
+
 		for (unsigned int q = 0; q < n_q_points; ++q) {
 			for (unsigned int k = 0; k < dofs_per_cell; ++k) {
 				div_phi_u[k] = fe_values[velocities].divergence(k, q);
@@ -106,14 +136,27 @@ void StationaryNavierStokes<dim>::assemble(const bool initial_step, const bool a
 			for (unsigned int i = 0; i < dofs_per_cell; ++i) {
 				if (assemble_matrix) {
 					for (unsigned int j = 0; j < dofs_per_cell; ++j) {
-						local_matrix(i, j) += (viscosity * scalar_product(grad_phi_u[i], grad_phi_u[j]) + phi_u[i] * (present_velocity_gradients[q] * phi_u[j]) + phi_u[i] * (grad_phi_u[j] * present_velocity_values[q]) - div_phi_u[i] * phi_p[j] - phi_p[i] * div_phi_u[j] + gamma * div_phi_u[i] * div_phi_u[j] + phi_p[i] * phi_p[j]) * fe_values.JxW(q);
+						local_matrix(i, j) += (viscosity * scalar_product(grad_phi_u[i], grad_phi_u[j])
+						 						+ phi_u[i] * (present_velocity_gradients[q] * phi_u[j]) 
+												+ phi_u[i] * (grad_phi_u[j] * present_velocity_values[q])
+												- div_phi_u[i] * phi_p[j] - phi_p[i] * div_phi_u[j] 
+												+ gamma * div_phi_u[i] * div_phi_u[j] + phi_p[i] * phi_p[j])
+												* fe_values.JxW(q);
 					}
 				}
 				double present_velocity_divergence = trace(present_velocity_gradients[q]);
-				local_rhs(i) += (-viscosity * scalar_product(grad_phi_u[i], present_velocity_gradients[q]) - phi_u[i] * (present_velocity_gradients[q] * present_velocity_values[q]) + div_phi_u[i] * present_pressure_values[q] + phi_p[i] * present_velocity_divergence - gamma * div_phi_u[i] * present_velocity_divergence) * fe_values.JxW(q);
+				local_rhs(i) += (-viscosity * scalar_product(grad_phi_u[i], present_velocity_gradients[q]) 
+								- phi_u[i] * (present_velocity_gradients[q] * present_velocity_values[q]) 
+								+ div_phi_u[i] * present_pressure_values[q]
+								+ phi_p[i] * present_velocity_divergence
+								- gamma * div_phi_u[i] * present_velocity_divergence) 
+								* fe_values.JxW(q);
 			}
 		}
 		cell->get_dof_indices(local_dof_indices);
+		
+		// this object here holds a list on constraint based on the fact wheter
+		// this is the initial step or not.
 		const AffineConstraints<double> &constraints_used = initial_step ? nonzero_constraints : zero_constraints;
 		if (assemble_matrix) {
 			constraints_used.distribute_local_to_global(local_matrix, local_rhs, local_dof_indices, system_matrix, system_rhs);
@@ -124,6 +167,8 @@ void StationaryNavierStokes<dim>::assemble(const bool initial_step, const bool a
 	if (assemble_matrix) {
 		pressure_mass_matrix.reinit(sparsity_pattern.block(1, 1));
 		pressure_mass_matrix.copy_from(system_matrix.block(1, 1));
+
+		// bottom right block of the system block has to be zero
 		system_matrix.block(1, 1) = 0;
 	}
 }
@@ -143,29 +188,51 @@ void StationaryNavierStokes<dim>::assemble_rhs(const bool initial_step)
 template <int dim>
 void StationaryNavierStokes<dim>::solve(const bool initial_step)
 {
+	// as before we define contraints bsased on the iteration we're on
 	const AffineConstraints<double> &constraints_used = initial_step ? nonzero_constraints : zero_constraints;
+
+	// initialize object for solving the system
 	SolverControl solver_control(system_matrix.m(), 1e-4 * system_rhs.l2_norm(), true);
 	SolverFGMRES<BlockVector<double>> gmres(solver_control);
+	
+	// initialize ILU preconditioner with the pressure mass matrix we derived in the assemble() function
 	SparseILU<double> pmass_preconditioner;
 	pmass_preconditioner.initialize(pressure_mass_matrix, SparseILU<double>::AdditionalData());
+
+	// initialize BlockShurPreconditioner passing the previously computed pmass precondtioner;
 	const BlockSchurPreconditioner<SparseILU<double>> preconditioner(gamma, viscosity, system_matrix, pressure_mass_matrix, pmass_preconditioner);
+	
+	// solve using the Shur Preconditioner
 	gmres.solve(system_matrix, newton_update, system_rhs, preconditioner);
 	std::cout << "FGMRES steps: " << solver_control.last_step() << std::endl;
 	constraints_used.distribute(newton_update);
 }
 
+/** @brief Function identifies area where the error is larger and refines the mesh
+ *  in order to create new cells there
+ */
 template <int dim>
 void StationaryNavierStokes<dim>::refine_mesh()
 {
+	// error estimation
 	Vector<float> estimated_error_per_cell(triangulation.n_active_cells());
 	const FEValuesExtractors::Vector velocity(0);
 	KellyErrorEstimator<dim>::estimate(dof_handler, QGauss<dim - 1>(degree + 1), std::map<types::boundary_id, const Function<dim> *>(), present_solution, estimated_error_per_cell, fe.component_mask(velocity));
+
+	// here it takes the 0.3 (30%) of the cells with the highest error from the triangulation for refinement
 	GridRefinement::refine_and_coarsen_fixed_number(triangulation, estimated_error_per_cell, 0.3, 0.0);
 	triangulation.prepare_coarsening_and_refinement();
+
+	// creating new points means it is necessary to add more dof.
+	// This also means we need to store somewhere the solutiuon computed so far
 	SolutionTransfer<dim, BlockVector<double>> solution_transfer(dof_handler);
 	solution_transfer.prepare_for_coarsening_and_refinement(present_solution);
 	triangulation.execute_coarsening_and_refinement();
+
+	// redefines dof
 	setup_dofs();
+
+	// interpolating on the new mesh
 	BlockVector<double> tmp(dofs_per_block);
 	solution_transfer.interpolate(tmp);
 	nonzero_constraints.distribute(tmp);
@@ -173,6 +240,9 @@ void StationaryNavierStokes<dim>::refine_mesh()
 	present_solution = tmp;
 }
 
+/** @brief We use the Newton method in order to solve a nonlinear system like
+ * 	this one 
+ */
 template <int dim>
 void StationaryNavierStokes<dim>::newton_iteration(const double tolerance, const unsigned int max_n_line_searches, const unsigned int max_n_refinements, const bool is_initial_step, const bool output_result)
 {
@@ -182,12 +252,16 @@ void StationaryNavierStokes<dim>::newton_iteration(const double tolerance, const
 		double last_res = 1.0;
 		double current_res = 1.0;
 		std::cout << "grid refinements: " << refinement_n << std::endl << "viscosity: " << viscosity << std::endl;
+
+		// main loop here that controls the nonlinear solver given a tolerance
 		while ((first_step || (current_res > tolerance)) && line_search_n < max_n_line_searches) {
 			if (first_step) {
+				// initialize and assemble the system in the first iter
 				setup_dofs();
 				initialize_system();
 				evaluation_point = present_solution;
 				assemble_system(first_step);
+
 				solve(first_step);
 				present_solution = newton_update;
 				nonzero_constraints.distribute(present_solution);
@@ -201,6 +275,9 @@ void StationaryNavierStokes<dim>::newton_iteration(const double tolerance, const
 				evaluation_point = present_solution;
 				assemble_system(first_step);
 				solve(first_step);
+
+				// interesting backtracking approach, instead of taking a fixed alpha for the newton iter it halves
+				// it each time				
 				for (double alpha = 1.0; alpha > 1e-5; alpha *= 0.5) {
 					evaluation_point = present_solution;
 					evaluation_point.add(alpha, newton_update);
@@ -216,7 +293,7 @@ void StationaryNavierStokes<dim>::newton_iteration(const double tolerance, const
 					std::cout << "  number of line searches: " << line_search_n << "  residual: " << current_res << std::endl;
 					last_res = current_res;
 				}
-				++line_search_n;
+				++line_search_n; // increment line search number
 			}
 			if (output_result) {
 				output_results(max_n_line_searches * refinement_n + line_search_n);
@@ -230,6 +307,10 @@ void StationaryNavierStokes<dim>::newton_iteration(const double tolerance, const
 	}
 }
 
+/** @brief Function that computes the initial guess when the Reynolds number is over 1000
+ * I guess we will not need this as in the paper it is written we are just working with
+ * Reynolds number smaller than 200.
+ */
 template <int dim>
 void StationaryNavierStokes<dim>::compute_initial_guess(double step_size)
 {
@@ -243,21 +324,35 @@ void StationaryNavierStokes<dim>::compute_initial_guess(double step_size)
 	}
 }
 
+
+/** @brief Function that just prints the output (pretty similar to Bucelli's) 
+*/
 template <int dim>
 void StationaryNavierStokes<dim>::output_results(const unsigned int output_index) const
 {
+	// vector of strings with dim element as "velocity" and last element "pressure"
 	std::vector<std::string> solution_names(dim, "velocity");
 	solution_names.emplace_back("pressure");
+
 	std::vector<DataComponentInterpretation::DataComponentInterpretation> data_component_interpretation(dim, DataComponentInterpretation::component_is_part_of_vector);
 	data_component_interpretation.push_back(DataComponentInterpretation::component_is_scalar);
+
 	DataOut<dim> data_out;
 	data_out.attach_dof_handler(dof_handler);
 	data_out.add_data_vector(present_solution, solution_names, DataOut<dim>::type_dof_data, data_component_interpretation);
 	data_out.build_patches();
-	std::ofstream output(std::to_string(1.0 / viscosity) + "-solution-" + Utilities::int_to_string(output_index, 4) + ".vtk");
+
+	const std::string output_file_name = std::to_string(1.0 / viscosity) + "-solution"
+	std::ofstream output(output_file_name + Utilities::int_to_string(output_index, 4) + ".vtk");
 	data_out.write_vtk(output);
+
+	std::out << "Output written to " << output_file_name << "." << std::endl;
+
 }
 
+/** @brief Function that it samples the velocity profile along the vertical centerline
+ *  of the domain and writes it to a text file. Usefull for testing and validation purposes.
+ */
 template <int dim>
 void StationaryNavierStokes<dim>::process_solution(unsigned int refinement)
 {
@@ -267,9 +362,13 @@ void StationaryNavierStokes<dim>::process_solution(unsigned int refinement)
 	p[0] = 0.5;
 	p[1] = 0.5;
 	f << std::scientific;
+
+	// samples specifically 101 points
 	for (unsigned int i = 0; i <= 100; ++i) {
 		p[dim - 1] = i / 100.0;
 		Vector<double> tmp_vector(dim + 1);
+
+		// given p it calculates the value of the present solution and stores it in tmp vector
 		VectorTools::point_value(dof_handler, present_solution, p, tmp_vector);
 		f << p[dim - 1];
 		for (int j = 0; j < dim; ++j)
@@ -278,6 +377,10 @@ void StationaryNavierStokes<dim>::process_solution(unsigned int refinement)
 	}
 }
 
+/** @brief Method that actually run the whole method calling the newton iteration.  
+ *  With really high Reynolds Number (not the case for us I guess) it also computes
+ *  an initial guess calling the "compute_initial_guess" function.
+*/
 template <int dim>
 void StationaryNavierStokes<dim>::run(const unsigned int refinement)
 {
