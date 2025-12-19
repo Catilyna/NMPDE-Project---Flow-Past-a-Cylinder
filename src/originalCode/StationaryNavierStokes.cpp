2336fcc25d61e191890025fe0d465765a16c7e7d
grimaldiTob <forzamilan688@gmail.com>
2025-12-19 17:06:42 +0100

    SNS: adapted output result function for parallelization

--- src/originalCode/StationaryNavierStokes.cpp
+++ src/originalCode/StationaryNavierStokes.cpp
@@ -386,66 +386,61 @@ namespace NavierStokes{
 	 * 	this one 
 	 */
 	template <int dim>
-	void StationaryNavierStokes<dim>::newton_iteration(const double tolerance, const unsigned int max_n_line_searches, const unsigned int max_n_refinements, const bool is_initial_step, const bool output_result)
+	void StationaryNavierStokes<dim>::newton_iteration(const double tolerance, const unsigned int max_n_line_searches, const bool is_initial_step, const bool output_result)
 	{
 		bool first_step = is_initial_step;
-		for (unsigned int refinement_n = 0; refinement_n < max_n_refinements + 1; ++refinement_n) {
-			unsigned int line_search_n = 0;
-			double last_res = 1.0;
-			double current_res = 1.0;
-			std::cout << "grid refinements: " << refinement_n << std::endl << "viscosity: " << viscosity << std::endl;
-	
-			// main loop here that controls the nonlinear solver given a tolerance
-			while ((first_step || (current_res > tolerance)) && line_search_n < max_n_line_searches) {
-				if (first_step) {
-					// initialize and assemble the system in the first iter
-					setup_dofs();
-					initialize_system();
+		unsigned int line_search_n = 0;
+		double last_res = 1.0;
+		double current_res = 1.0;
+
+		// main loop here that controls the nonlinear solver given a tolerance
+		while ((first_step || (current_res > tolerance)) && line_search_n < max_n_line_searches) {
+			if (first_step) {
+				// initialize and assemble the system in the first iter
+				setup_dofs();
+				initialize_system();
+				evaluation_point = present_solution;
+				assemble_system(first_step);
+
+				solve(first_step);
+				present_solution = newton_update;
+				nonzero_constraints.distribute(present_solution);
+				first_step = false;
+				evaluation_point = present_solution;
+				assemble_rhs(first_step);
+				current_res = system_rhs.l2_norm();
+				std::cout << "The residual of initial guess is " << current_res << std::endl;
+				last_res = current_res;
+			} else {
+				evaluation_point = present_solution;
+				assemble_system(first_step);
+				solve(first_step);
+
+				// interesting backtracking approach, instead of taking a fixed alpha for the newton iter it halves
+				// it each time				
+				for (double alpha = 1.0; alpha > 1e-5; alpha *= 0.5) {
 					evaluation_point = present_solution;
-					assemble_system(first_step);
-	
-					solve(first_step);
-					present_solution = newton_update;
-					nonzero_constraints.distribute(present_solution);
-					first_step = false;
-					evaluation_point = present_solution;
+					evaluation_point.add(alpha, newton_update);
+					nonzero_constraints.distribute(evaluation_point);
 					assemble_rhs(first_step);
 					current_res = system_rhs.l2_norm();
-					std::cout << "The residual of initial guess is " << current_res << std::endl;
-					last_res = current_res;
-				} else {
-					evaluation_point = present_solution;
-					assemble_system(first_step);
-					solve(first_step);
-	
-					// interesting backtracking approach, instead of taking a fixed alpha for the newton iter it halves
-					// it each time				
-					for (double alpha = 1.0; alpha > 1e-5; alpha *= 0.5) {
-						evaluation_point = present_solution;
-						evaluation_point.add(alpha, newton_update);
-						nonzero_constraints.distribute(evaluation_point);
-						assemble_rhs(first_step);
-						current_res = system_rhs.l2_norm();
-						std::cout << "  alpha: " << std::setw(10) << alpha << std::setw(0) << "  residual: " << current_res << std::endl;
-						if (current_res < last_res)
-							break;
-					}
-					{
-						present_solution = evaluation_point;
-						std::cout << "  number of line searches: " << line_search_n << "  residual: " << current_res << std::endl;
-						last_res = current_res;
-					}
-					++line_search_n; // increment line search number
+					std::cout << "  alpha: " << std::setw(10) << alpha << std::setw(0) << "  residual: " << current_res << std::endl;
+					if (current_res < last_res)
+						break;
 				}
+				{
+					present_solution = evaluation_point;
+					std::cout << "  number of line searches: " << line_search_n << "  residual: " << current_res << std::endl;
+					last_res = current_res;
+				}
+				++line_search_n; // increment line search number
+			}
+				/*
 				if (output_result) {
-					output_results(max_n_line_searches * refinement_n + line_search_n);
+					output_results(line_search_n);
 					if (current_res <= tolerance)
 						process_solution();
-				}
-			}
-			if (refinement_n < max_n_refinements) {
-				refine_mesh();
-			}
+				}*/ 
 		}
 	}
 	
@@ -470,8 +465,10 @@ namespace NavierStokes{
 	/** @brief Function that just prints the output (pretty similar to Bucelli's) 
 	*/
 	template <int dim>
-	void StationaryNavierStokes<dim>::output_results(const unsigned int output_index) const
+	void StationaryNavierStokes<dim>::output_results() const
 	{
+	    pcout << "===============================================" << std::endl;
+
 		// vector of strings with dim element as "velocity" and last element "pressure"
 		std::vector<std::string> solution_names(dim, "velocity");
 		solution_names.emplace_back("pressure");
@@ -480,18 +477,27 @@ namespace NavierStokes{
 		data_component_interpretation.push_back(DataComponentInterpretation::component_is_scalar);
 	
 		DataOut<dim> data_out;
-		data_out.attach_dof_handler(dof_handler);
-		data_out.add_data_vector(present_solution, solution_names, DataOut<dim>::type_dof_data, data_component_interpretation);
+  		data_out.add_data_vector(dof_handler, 
+								solution, 
+								solution_names, 
+								data_component_interpretation);
+
+		std::vector<unsigned int> partition_int(mesh.n_active_cells());
+		GridTools::get_subdomain_association(mesh, partition_int);
+		const Vector<double> partitioning(partition_int.begin(), partition_int.end());
+		data_out.add_data_vector(partitioning, "partitioning");
 		data_out.build_patches();
 	
 		// here to insert correct ReyNolds Number aswell REMEMBER THIS 
 		const std::string output_file_name = std::to_string(1.0 / viscosity) + "-solution";
 		data_out.write_vtu_with_pvtu_record("../results",
-                                      output_file_name,
-                                      0,
-                                      MPI_COMM_WORLD);
+											output_file_name,
+											0,
+											MPI_COMM_WORLD);
 	
 		std::cout << "Output written to " << output_file_name << "." << std::endl;
+		pcout << "===============================================" << std::endl;
+
 	}
 	
 	/** @brief Function that it samples the velocity profile along the vertical centerline
